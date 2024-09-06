c794ec54ffded124f92ea646cd9ae287274052c9
Jérémie Noah Chevalley <pb83pyi@eduvaud.ch>
2024-09-06 14:50:34 +0200

    feat: did the switch exercise

---
+++ src/weeklyDataSwitchCase.js
@@ -0,0 +1,19 @@
+
+let i = 1
+switch (i){
+    case 1:
+        day = "Monday"; break
+    case 2:
+        day = "Tuesday"; break
+    case 3:
+        day = "Wednesday"; break
+    case 4:
+        day = "Thursday"; break
+    case 5:
+        day = "Friday"; break
+    case 6:
+        day = "Saturday"; break
+    case 7:
+        day = "Sunday"; break
+}
+console.log(day); //Monday
